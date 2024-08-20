798f0a498fc58bec25057f14b79d91e0c5b94671
palraj007 <84125074+palraj007@users.noreply.github.com>
2024-08-20 12:29:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,163 +20,25 @@ if(navClose){
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
-
-function toggleSkills(){
-    let itemClass = this.parentNode.className
-
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass == 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
-    }
-}
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click', toggleSkills)
-})
 
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content')
-
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
-
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification__active')
-        })
-        target.classList.add('qualification__active')
-
-        tabs.forEach(tab =>{
-            tab.classList.remove('qualification__active')
-        })
-        tab.classList.add('qualification__active')
-    })
-})
 
 
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalClose = document.querySelectorAll('.services__modal-close')
-
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
-modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () =>{
-        modal(i)
-    })
-})
-
-
-modalClose.forEach((modalClose) => {
-    modalClose.addEventListener('click', () =>{
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop: true,
 
-    navigation: {
-        nextE1: '.swiper-button-next',
-        prevE1: '.swiper-button-prev',
-    },
-    pagination: {
-        e1: '.swiper-pagination',
-        clickable: true,
-    },
- 
-});
 
 /*==================== TESTIMONIAL email js ====================*/
-const contactForm = document.getElementById('contact-form')
-        contactMessage = document.getElementById('contact-message')
-
-const sendEmail = (e) =>{
-    e.preventDefault()
-        // serviceid templateid #form publickey
-    emailjs.sendForm('service_4yhwewc','template_c2kureg','#contact-form','v9JgqZcb2XfTdb9SC')
-        .then(() =>{
-            contactMessage.textContent = 'Message sent successfully '
-
-            setTimeout(() =>{
-                contactMessage.textContent = ''
-            }, 5000)
-
-            contactForm.reset()
-        }, () => {
-            contactMessage.textContent = 'Message not sent '
-        })
-}
-contactForm.addEventListener('submit', sendEmail)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-    function scrollActive(){
-        const scrollY = window.pageYOffset
 
-        sections.forEach(current =>{
-            const sectionHeight = current.offsetHeight
-            const sectionTop = current.offsetTop - 50;
-            sectionId = current.getAttribute('id')
-
-            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-            }else{
-                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-            }
-        })
-    }
-window.addEventListener('scroll', scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
-    const nav = document.getElementById('header')
 
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up')
-
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
-
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-const getCurrentTheme = () => document.body.classList(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList(iconTheme) ? 'uil-moon' : 'uil-sun'
-
-if(selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-}
 
 themeButton.addEventListener('click', () => {
 
